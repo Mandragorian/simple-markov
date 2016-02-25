ab4b33b6ed0d9500402dab00228373844b195340
mandragore <gkonstandinos@gmail.com>
2016-02-25 16:42:36 +0200

    Add code for python2 compatibility

---
+++ utils.py
@@ -0,0 +1,14 @@
+import operator
+def accumulate(iterable, func=operator.add):
+    'Return running totals'
+    # accumulate([1,2,3,4,5]) --> 1 3 6 10 15
+    # accumulate([1,2,3,4,5], operator.mul) --> 1 2 6 24 120
+    it = iter(iterable)
+    try:
+        total = next(it)
+    except StopIteration:
+        return
+    yield total
+    for element in it:
+        total = func(total, element)
+        yield total
